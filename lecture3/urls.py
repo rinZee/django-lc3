baf8170f6e98ead52378c50975cda40fbe21aaaf
unknown <trex33160@gmail.com>
2021-08-02 22:21:38 +0545

    added datetime

--- lecture3/urls.py
+++ lecture3/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^hello/', include("hello.urls"))
+    url(r'^hello/', include("hello.urls")),
+    url(r'^newyear/', include("newyear.urls"))
 ]
-
+ 
